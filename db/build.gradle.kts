0383d94267b1d3b0d7d750d9d388942ba58cd9c8
guilherme lira <guilherme.lira@ifood.com>
2023-06-30 18:51:25 -0300

    git ignore

---
+++ db/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.serpro69:kotlin-faker:1.14.0")
+    implementation("org.postgresql:postgresql:42.6.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ db/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val service = Service()
+    service.save()
+    println("Hello World!")
+}
